1a3dff4b7dafcd50363f8638bec1308156586a72
mikayladalton2 <103148825+mikayladalton2@users.noreply.github.com>
2025-05-21 11:00:08 -0700

    start of job-tracking app

---
+++ main.py
@@ -0,0 +1,101 @@
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from database import engine, SessionLocal
+import models
+from pydantic import BaseModel
+from typing import Optional
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow requests from any frontend
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all HTTP methods (GET, POST, DELETE, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class JobCreate(BaseModel):
+    company: str
+    position: str
+    status: str = "Applied"
+
+@app.get("/jobs")
+def get_jobs(
+    status: Optional[str] = None, # make status filter optional
+    sort: Optional[str] = None,
+    limit: int = 10,  # default limit to 10
+    page: int = 1, # default to page 1
+
+    db: Session = Depends(get_db)
+):
+    query = db.query(models.JobApplication)
+    if status:
+        query = query.filter(models.JobApplication.status == status)
+    if sort == "latest":
+        query = query.order_by(models.JobApplication.date_applied.desc())
+    offset = (page-1) * limit
+    return query.offset(offset).limit(limit).all()
+
+@app.post("/jobs")
+def create_job(job: JobCreate, db: Session = Depends(get_db)):
+    new_job = models.JobApplication(**job.dict())
+    db.add(new_job)
+    db.commit()
+    db.refresh(new_job)
+    return new_job
+
+@app.get("/jobs/{job_id}")
+def get_job(job_id: int, db: Session = Depends(get_db)):
+    job = db.query(models.JobApplication).filter(models.JobApplication.id == job_id).first()
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    return job
+
+class JobUpdate(BaseModel):
+    status: str
+
+@app.put("/jobs/{job_id}")
+def udpate_job(job_id: int, job_update: JobUpdate, db: Session = Depends(get_db)):
+    job = db.query(models.JobApplication).filter(models.JobApplication.id == job_id).first()
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    job.status = job_update.status
+    db.commit()
+    db.refresh(job)
+    return job
+
+@app.delete("/jobs/{job_id}")
+def delete_job(job_id: int, db: Session = Depends(get_db)):
+    job = db.query(models.JobApplication).filter(models.JobApplication.id == job_id).first()
+    if not job:
+        raise HTTPException(status_code=404, detail="Job not found")
+    db.delete(job)
+    db.commit()
+    return{"message": "Job successfully removed"}
+
+
+
+@app.get("/")
+def read_root():
+    return {"message": "Job Tracker API is running!"}
+
+@app.get("/jobs")
+def get_jobs(db: Session = Depends(get_db)):
+    return db.query(models.JobApplication).all()
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="127.0.0.1", port=8080)
---
+++ models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+import datetime
+
+Base = declarative_base()
+
+class JobApplication(Base):
+    __tablename__ = "job_applications"
+
+    id = Column(Integer, primary_key=True, index=True)
+    company = Column(String, index=True)
+    position = Column(String)
+    status = Column(String, default="Applied", index=True)  # Applied, Interviewed, Offer, Rejected
+    date_applied = Column(DateTime, default=datetime.datetime.utcnow, index=True)
